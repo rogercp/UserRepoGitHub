ca1707dfe6f3c42a4045f49a602c10bc5eed315b
rogercp <rogercp1234@yahoo.com>
2020-10-09 22:22:36 -0700

    gsap color methods for switch

--- sorting/src/components/Insertion.js
+++ sorting/src/components/Insertion.js
@@ -23,10 +23,16 @@ const randomListOfNums =(n)=>
 function Insertion() {
 
     const [values, setValues] = useState(randomListOfNums(20))
+    const [mobileValues, setMobileValues] = useState(randomListOfNums(20))
+
 
     const revealsRef = React.useRef([]);
-
     revealsRef.current = [];
+
+    const revealsRefMobile= React.useRef([]);
+    revealsRefMobile.current = [];
+
+
 
     const useEffect=(()=>
     {
@@ -45,57 +51,72 @@ const addToRefs = (el) =>
         revealsRef.current.push(el);
     }
 
-    console.log(revealsRef.current)
+}
+
+const addToRefsMobile = (el) =>
+{
+
+    if(el && !revealsRefMobile.current.includes(el))
+    {
+        revealsRefMobile.current.push(el);
+    }
 }
 
 
 const onAnimate1 = () => {
 
-    const rect = revealsRef.current[0].getBoundingClientRect();
-    console.log(rect.top, rect.right, rect.bottom, rect.left)
 
-    const rect2 = revealsRef.current[10].getBoundingClientRect();
-    console.log(rect2.top, rect2.right, rect2.bottom, rect2.left)
+        const rect = revealsRef.current[0].getBoundingClientRect();
+        console.log(rect.top, rect.right, rect.bottom, rect.left)
 
+        const rect2 = revealsRef.current[10].getBoundingClientRect();
+        console.log(rect2.top, rect2.right, rect2.bottom, rect2.left)
 
-    let tl = gsap.timeline(); 
+        let tl = gsap.timeline(); 
 
+        let another = gsap.timeline();
 
-    tl.to(revealsRef.current[0], 0.9, {y: 400,delay: 0.5,backgroundColor:"red"})
-    .to(revealsRef.current[10], 0.9, {y: 400,delay: 0.5,backgroundColor:"red"})
-
-    .to(revealsRef.current[0], 0.9, {x:  (rect2.right-rect.right),delay: 0.5})
-    .to(revealsRef.current[10], 0.9, {x: -(rect2.right-rect.right) ,delay: 0.5})
+        tl.to(revealsRef.current[0], 0.9, {y: 400,delay: 0.5,backgroundColor:"red"})
+        .to(revealsRef.current[10], 0.9, {y: 400,delay: 0.5,backgroundColor:"red"})
     
-    .to(revealsRef.current[0], 0.9, {y: -(rect2.bottom-rect2.top+ revealsRef.current[10]),delay: 0.5,backgroundColor:"white"})
-    .to(revealsRef.current[10], 0.9, {y: -(rect.bottom-rect.top +revealsRef.current[0]),delay: 0.5,backgroundColor:"white"});
+        .to(revealsRef.current[0], 0.9, {x:  (rect2.right-rect.right),delay: 0.5},"-=.5")
+        .to(revealsRef.current[10], 0.9, {x: -(rect2.right-rect.right) ,delay: 0.5},"-=.5")
+        
+        .to(revealsRef.current[0], 0.9, {y: -(rect2.bottom-rect2.top+ revealsRef.current[10]),delay: 0.5,backgroundColor:"cadetblue"})
+        .to(revealsRef.current[10], 0.9, {y: -(rect.bottom-rect.top +revealsRef.current[0]),delay: 0.5,backgroundColor:"cadetblue"});
+    
 
+   
 
 
 
 }
+
 
 
 const onAnimate2 = () => {
 
-    const rect = revealsRef.current[0].getBoundingClientRect();
+
+
+    const rect = revealsRefMobile.current[0].getBoundingClientRect();
     console.log(rect.top, rect.right, rect.bottom, rect.left)
 
-    const rect2 = revealsRef.current[10].getBoundingClientRect();
+    const rect2 = revealsRefMobile.current[10].getBoundingClientRect();
     console.log(rect2.top, rect2.right, rect2.bottom, rect2.left)
 
 
     let tl = gsap.timeline(); 
 
 
-    tl.to(revealsRef.current[0], 0.9, {x: 200,delay: 0.5,backgroundColor:"red"})
-    .to(revealsRef.current[10], 0.9, {x: 200,delay: 0.5,backgroundColor:"red"})
+    tl.to(revealsRefMobile.current[0], 0.9, {x: 200,delay: 0.5,backgroundColor:"red"})
+    .to(revealsRefMobile.current[10], 0.9, {x: 200,delay: 0.5,backgroundColor:"red"})
 
-    .to(revealsRef.current[0], 0.9, {y:  (rect2.bottom-rect.bottom),delay: 0.5})
-    .to(revealsRef.current[10], 0.9, {y: -(rect2.bottom-rect.bottom) ,delay: 0.5})
+    .to(revealsRefMobile.current[0],  0.9,{y:  (rect2.bottom-rect.bottom),delay: 0.5},"-=.5")
+    .to(revealsRefMobile.current[10],  0.9,{y: -(rect2.bottom-rect.bottom) ,delay: 0.5},"-=.5")
     
-    .to(revealsRef.current[0], 0.9, {x: -(rect2.bottom-rect2.top+ revealsRef.current[10]),delay: 0.5,backgroundColor:"white"})
-    .to(revealsRef.current[10], 0.9, {x: -(rect.bottom-rect.top +revealsRef.current[0]),delay: 0.5,backgroundColor:"white"});
+    .to(revealsRefMobile.current[0], 0.9, {x: -(rect2.bottom-rect2.top+ revealsRefMobile.current[10]),delay: 0.5,backgroundColor:"cadetblue"})
+    .to(revealsRefMobile.current[10], 0.9, {x: -(rect.bottom-rect.top +revealsRef.current[0]),delay: 0.5,backgroundColor:"cadetblue"});
+
 
 
 
@@ -105,53 +126,53 @@ const onAnimate2 = () => {
 
 
 
-const onAnimate = () => {
+// const onAnimate = () => {
 
 
-    let tl = gsap.timeline(); 
+//     let tl = gsap.timeline(); 
   
 
-    for(let i = 0 ;i<revealsRef.current.length;i++)
-    {
+//     for(let i = 0 ;i<revealsRef.current.length;i++)
+//     {
         
-        tl.to(revealsRef.current[i], 0.1, {y: 100,backgroundColor:"red",duration:2})
-        .to(revealsRef.current[i], 0.1, {y: -10,backgroundColor:"white",duration:2});
-    }
+//         tl.to(revealsRef.current[i], 0.1, {y: 100,backgroundColor:"red",duration:2})
+//         .to(revealsRef.current[i], 0.1, {y: -10,backgroundColor:"white",duration:2});
+//     }
 
     
-    for(let i = 0 ;i<revealsRef.current.length;i++)
-    {
+//     for(let i = 0 ;i<revealsRef.current.length;i++)
+//     {
         
-        tl.to(revealsRef.current[i], 0.01, {y: 100,backgroundColor:"red",duration:2})
-        .to(revealsRef.current[i], 0.01, {y: -10,backgroundColor:"white",duration:2});
+//         tl.to(revealsRef.current[i], 0.01, {y: 100,backgroundColor:"red",duration:2})
+//         .to(revealsRef.current[i], 0.01, {y: -10,backgroundColor:"white",duration:2});
 
-    }
+//     }
 
-}
+// }
 
 
 
-const onAnimateMobile = () => {
+// const onAnimateMobile = () => {
 
-    let tl = gsap.timeline(); 
+//     let tl = gsap.timeline(); 
 
-    for(let i = 0 ;i<revealsRef.current.length;i++)
-    {
+//     for(let i = 0 ;i<revealsRefMobile.current.length;i++)
+//     {
         
-        tl.to(revealsRef.current[i], 0.1, {x: 100,backgroundColor:"red",duration:2})
-        .to(revealsRef.current[i], 0.1, {x: -10,backgroundColor:"white",duration:2});
+//         tl.to(revealsRefMobile.current[i], 0.1, {x: 100,backgroundColor:"red",duration:2})
+//         .to(revealsRefMobile.current[i], 0.1, {x: -10,backgroundColor:"white",duration:2});
 
-    }
+//     }
 
-    for(let i = 0 ;i<revealsRef.current.length;i++)
-    {
+//     for(let i = 0 ;i<revealsRefMobile.current.length;i++)
+//     {
         
-        tl.to(revealsRef.current[i], 0.01, {x: 100,backgroundColor:"red",duration:2})
-        .to(revealsRef.current[i], 0.01, {x: -10,backgroundColor:"white",duration:2});
+//         tl.to(revealsRefMobile.current[i], 0.01, {x: 100,backgroundColor:"red",duration:2})
+//         .to(revealsRefMobile.current[i], 0.01, {x: -10,backgroundColor:"white",duration:2});
 
-    }
+//     }
 
-}
+// }
 
 
 
@@ -170,7 +191,7 @@ const onAnimateMobile = () => {
          {values.map((num,index)=>{
 
             return (
-                <div className="blocksNumber"  style={{display:"flex", flexDirection:"column",width:"100%",alignItems:"center"}}>
+                <div className="blocksNumber"  key={num} ref={addToRefs}  style={{display:"flex", flexDirection:"column",width:"100%",alignItems:"center"}}>
 
                 
                 <div>
@@ -180,13 +201,12 @@ const onAnimateMobile = () => {
                 </div>
 
                 <div 
-                key={num}
-                ref={addToRefs}
+               
                 className="block"
                 style={{height:`${num*10}px`, backgroundColor:"ivory",margin:"5px",width:"80%"}}
-                onClick={onAnimate}
+            
                 >
-                    
+
                 </div>
                 </div>
             )
@@ -200,7 +220,7 @@ const onAnimateMobile = () => {
             {values.map((num,index)=>{
 
             return (
-                <div className="blocksNumber"  style={{display:"flex", flexDirection:"row",width:"100%", alignItems:"center",margin:"0",padding:"0"}}>
+                <div className="blocksNumber"   key={num} ref={addToRefsMobile} style={{display:"flex", flexDirection:"row",width:"100%", alignItems:"center",margin:"0",padding:"0"}}>
 
                 <div style={{width:"10%"}}>
 
@@ -209,13 +229,11 @@ const onAnimateMobile = () => {
                 </div>
 
                 <div 
-                key={num}
-                ref={addToRefs}
+
                 className="block"
                 style={{width:`${num*6}px`, backgroundColor:"ivory",marginLeft:"5px",padding:"0",height:"10px"}}
-                onClick={onAnimateMobile}
+                
                 >
-                    
                     
                 </div>
                 </div>
