f7f618c6ab7625563ad76681ca8a47eaaacb65e1
rogercp <rogercp1234@yahoo.com>
2020-10-19 22:16:34 -0700

    need responsive chart

--- sorting/src/components/TopUserLanguages.js
+++ sorting/src/components/TopUserLanguages.js
@@ -5,32 +5,34 @@ import { RadialChart} from 'react-vis';
 import '../App.css';
 
 const useStyles = makeStyles(theme => ({
-   
     paper: {
-      height: '100%',
-      backgroundColor: theme.palette.background.paper,
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(0, 0, 0),
-      outline: 'none',
-      margin: '1%',
-      flexDirection: "column",
-      justifyContent: "space-between",
-      alignItems: "center",
-      boxShadow: "0 16px 19px rgba(0,0,0,0.2), 0 15px 15px rgba(0,0,0,0.2)",
-      '&:hover': {
-        boxShadow: "0 2px 4px rgba(0,0,0,0.25), 0 2px 2px rgba(0,0,0,0.22)"
-      },
-      [theme.breakpoints.down('md')]: {
-        width: '100%',
         height: '100%',
-      },
-      [theme.breakpoints.down('sm')]: {
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
         padding: theme.spacing(0, 0, 0),
-        width: '100%',
-        height: '100%',
+        outline: 'none',
+        margin: '1%',
+        display:"block",
+        justifyContent: "center",
+        alignItems: "center",
+        boxShadow: "0 16px 19px rgba(0,0,0,0.2), 0 15px 15px rgba(0,0,0,0.2)",
+        '&:hover': {
+          boxShadow: "0 2px 4px rgba(0,0,0,0.25), 0 2px 2px rgba(0,0,0,0.22)"
+        },
+        [theme.breakpoints.down('md')]: {
+          width: '100%',
+          height: '100%',
+        },
+        [theme.breakpoints.down('sm')]: {
+          padding: theme.spacing(0, 0, 0),
+          width: '100%',
+          height: '100%',
+        },
       },
-    },
   
+    section:{
+
+    }
   
   }))
 
@@ -45,18 +47,20 @@ function TopUserLanguages(props) {
 
     <Card className={classes.paper} style={{ border: "black", minWidth: "350px", minHeight: "325px", maxWidth: "350px" }}> 
 
-    <RadialChart
+    <section className = {classes.section}>
 
-    style={{margin: "0 auto"}}
+
+    <RadialChart
     colorType="literal"
     showLabels={true}
     data={myData}
     width={300}
     height={300} />
 
+    </section>
+
 
     </Card>
-
   );
 }
 
